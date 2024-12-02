6c665639d85c62f0fee177d1c6705f8a6442589b
armornick <nickstories@gmail.com>
2024-12-02 07:09:36 +0100

    Exercise 2: Keeping track of guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
+import GuessList from '../GuessList/GuessList';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,13 +11,20 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = useState([]);
   
   const submitGuess = (guess) => {
     console.log({ guess });
+    const newGuess = {
+      value: guess,
+      id: crypto.randomUUID(),
+    }
+    setGuesses([ ...guesses, newGuess ]);
   }
 
   return (
     <>
+      <GuessList guesses={guesses} />
       <GuessInput submitGuess={submitGuess} />
     </>
   );
